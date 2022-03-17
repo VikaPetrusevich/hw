8667901275983c08be6d54465228f90fdd8f780b
Vika Petrusevich <p.petrusevich@reply.com>
2022-03-17 10:54:32 +0300

    add homework 6, Task 1,2,3

---
+++ HW.06/HW.06.Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Creating arrays.");
+
+int[] numbers = new int[5];
+
+Random random = new Random();
+
+for (int i = 0; i < numbers.Length; i++) 
+{ 
+    numbers[i] = random.Next(1, 100);
+}
+
+Console.WriteLine("Read array №1.");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine(" ");
+
+int [] numbers2 = new int[5];
+
+for (int i = 0; i < numbers2.Length; i++)
+{
+    Console.WriteLine("Please, input a number: ");
+    string text = Console.ReadLine(); 
+    int num = Convert.ToInt32(text);
+    numbers2[i] = num;
+}
+
+Console.WriteLine("Read array №2.");
+for (int i = 0; i < numbers2.Length; i++)
+{
+    Console.Write(numbers2[i] + " ");
+}
+
+Console.WriteLine(" ");
+
+int [] numbers3 = new int[5];
+
+Console.WriteLine("Result os sum of arrays.");
+
+for (int i = 0; i < numbers3.Length; i++)
+{
+    numbers3[i] = numbers[i] + numbers2[i];
+    Console.Write(numbers3[i] + " ");
+}
+
+Console.ReadKey();
---
+++ HW.06/HW.06.Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Reverse array implementation.");
+Random random = new Random();
+int [] numbers = new int [10];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers [i] = random.Next (0, 100);
+}
+
+Console.WriteLine("Read array.");
+
+foreach (var item in numbers)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine("\nReverse array.");
+
+int [] numbers2 = new int [10];
+
+for (int i = 0; i < numbers2.Length; i++)
+{
+    numbers2 [numbers2.Length-1-i] = numbers [i];
+}
+
+Console.WriteLine("Read reverse array.");
+
+foreach (var item in numbers2)
+{
+    Console.Write(item + " ");
+}
+
+Console.ReadKey();
