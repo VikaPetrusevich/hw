9c57b9816434cc2ba69d63daa1eec10b67ad3125
Vika Petrusevich <p.petrusevich@reply.com>
2022-04-10 11:42:06 +0300

    add homework 12

---
+++ MotorcycleStore/Motorcycle.BL/IStoreService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorcycle.BL
+{
+    public interface IStoreService
+    {
+        void AddMoto(MotoModel motoModel);
+
+        MotoModel GetMotoById(Guid id);
+
+        List<MotoModel> GetMotoList();
+
+        void Delete(Guid id);
+
+        void Update(MotoModel motoModel);
+    }
+}
---
+++ MotorcycleStore/MotorcycleStore.UI/Program.cs
@@ -0,0 +1,100 @@
+﻿using Motorcycle.BL;
+
+var motoModel = new MotoModel()
+{
+    Id = Guid.NewGuid(),
+    Name = "BMV",
+    Model = "X350",
+    Year = 2021,
+    Odometer = 65
+};
+
+var motoModel2 = new MotoModel()
+{
+    Id = Guid.NewGuid(),
+    Name = "Honda",
+    Model = "M666",
+    Year = 2020,
+    Odometer = 90
+};
+
+var motoModel3 = new MotoModel()
+{
+    Id = Guid.NewGuid(),
+    Name = "Suzuki",
+    Model = "KL80",
+    Year = 2021,
+    Odometer = 70
+};
+
+var motoModel4 = new MotoModel()
+{
+    Id = Guid.NewGuid(),
+    Name = "Yamaha",
+    Model = "S500",
+    Year = 2022,
+    Odometer = 20
+};
+
+var motoModel5 = new MotoModel()
+{
+    Id = Guid.NewGuid(),
+    Name = "Kawasaki",
+    Model = "JK450",
+    Year = 2020,
+    Odometer = 90
+};
+
+IStoreService service = new StoreServis();
+
+service.AddMoto(motoModel);
+service.AddMoto(motoModel2);
+service.AddMoto(motoModel3);
+service.AddMoto(motoModel4);
+service.AddMoto(motoModel5);
+
+Console.WriteLine("Return all.");
+
+var motos = service.GetMotoList();
+
+foreach (var item in motos)
+{
+    Console.WriteLine(item.ToString());
+}
+
+Console.WriteLine("Return by Id.");
+
+var motoById = service.GetMotoById(motoModel3.Id);
+
+Console.WriteLine(motoById);
+
+Console.WriteLine("Delete by Id.");
+
+service.Delete(motoModel5.Id);
+
+motos = service.GetMotoList();
+
+foreach (var item in motos)
+{
+    Console.WriteLine(item.ToString());
+}
+
+Console.WriteLine("Update moto by Id.");
+
+var motoModel6 = new MotoModel()
+{
+    Id = motoModel4.Id,
+    Name = "Minsk",
+    Model = "A100",
+    Year = 2020,
+    Odometer = 100
+};
+
+service.Update(motoModel6);
+
+motos = service.GetMotoList();
+
+foreach (var item in motos)
+{
+    Console.WriteLine(item.ToString());
+}
